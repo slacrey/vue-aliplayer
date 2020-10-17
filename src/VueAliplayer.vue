138359ea7751847497d9dbe561352d5130109a2e
412845222 <412845222@qq.com>
2020-10-18 02:05:55 +0800

    master

--- src/VueAliplayer.vue
+++ src/VueAliplayer.vue
@@ -94,7 +94,28 @@
       skinLayout: {
         type: Array,
         default: function () {
-          return []
+          return [
+            { name: "bigPlayButton", align: "blabs", x: 30, y: 80 },
+            {
+              name: "H5Loading", align: "cc"
+            },
+            { name: "errorDisplay", align: "tlabs", x: 0, y: 0 },
+            { name: "infoDisplay" },
+            { name: "tooltip", align: "blabs", x: 0, y: 56 },
+            { name: "thumbnail" },
+            {
+              name: "controlBar", align: "blabs", x: 0, y: 0,
+              children: [
+                { name: "progress", align: "blabs", x: 0, y: 44 },
+                { name: "playButton", align: "tl", x: 15, y: 12 },
+                { name: "timeDisplay", align: "tl", x: 10, y: 7 },
+                { name: "fullScreenButton", align: "tr", x: 10, y: 12 },
+                { name: "subtitle", align: "tr", x: 15, y: 12 },
+                { name: "setting", align: "tr", x: 15, y: 12 },
+                { name: "volume", align: "tr", x: 5, y: 10 }
+              ]
+            }
+          ]
         }
       },
       //默认为‘hover’。可选的值为：‘click’、‘hover’、‘always’。
